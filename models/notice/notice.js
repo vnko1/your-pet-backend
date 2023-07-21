563b0b920d8b82ef5ff299663e5eb04e6255efaa
Butcherok <Butcherok231@gmail.com>
2023-07-21 14:24:17 +0300

    add: more fields for addSchema validation

--- models/notice/notice.js
+++ models/notice/notice.js
@@ -65,12 +65,15 @@ const addSchema = Joi.object({
 		.required(),
 	name: Joi.string().min(2).max(16).uppercase().required(),
 	date: Joi.date()
-		.format('DD-MM-YYYY')
+		.format("DD-MM-YYYY")
 		.min("01-01-2000")
 		.messages({ "date.format": `Date format is DD-MM-YYYY` })
 		.required(),
 	type: Joi.string().min(2).max(16).uppercase().required(),
+	sex: Joi.string().valid("male", "female").required(),
+  location: Joi.string().min(2).max(16).uppercase().required(),
 	price: Joi.number().integer().greater(0).required(),
+	comments: Joi.string().max(120).uppercase().required(),
 	favorite: Joi.boolean,
 });
 

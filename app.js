3f8a4c33d2512ebd77b38ea0fa65ad801f531f85
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-07-22 11:37:55 +0300

    add defauly avatar url

--- app.js
+++ app.js
@@ -16,11 +16,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use(
-  "/api-documentation",
-  swaggerUi.serve,
-  swaggerUi.setup(swaggerDocument)
-);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/users", usersRouter);
 

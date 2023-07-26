7dbea300dd36f517a560a9334a6a7481e1829664
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-07-26 07:00:09 +0300

    finish code refact

--- middlewares/isValdId.js
+++ middlewares/isValdId.js
@@ -1,12 +1,10 @@
 const { isValidObjectId } = require("mongoose");
 const { httpError } = require("../utils");
-const { errorMessage } = require("../constants");
 
-const isValidId = (req, _, next) => {
+const isValidId = (code, message) => (req, _, next) => {
   const key = Object.keys(req.params);
 
-  if (!isValidObjectId(req.params[key]))
-    next(httpError(400, errorMessage[400]));
+  if (!isValidObjectId(req.params[key])) next(httpError(code, message));
 
   next();
 };

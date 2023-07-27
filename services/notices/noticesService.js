a1c76774be88bc4c7d5a34be0a3d8b54ba4cc578
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-07-27 17:38:03 +0300

    add Search class

--- services/notices/noticesService.js
+++ services/notices/noticesService.js
@@ -1,4 +1,5 @@
 const { Notice } = require("../../models");
+const { Search } = require("../../utils");
 
 class Notices {
   static addNotice(newNotice) {
@@ -9,66 +10,23 @@ class Notices {
     return Notice.findById(id).populate("owner", "email phone");
   }
 
-  static async findAll({
-    filter = "",
-    category,
-    page = 1,
-    limit = 6,
-    sort = "desc",
-    sex,
-    date = 0,
-  }) {
-    const perPage = page > 0 ? (page - 1) * limit : 0;
-    const findOptions = filter
-      ? {
-          $or: [
-            { title: { $regex: filter, $options: "i" } },
-            { comments: { $regex: filter, $options: "i" } },
-          ],
-        }
-      : {};
+  static async findAll({ filter, category, sex, date, page, limit, sort }) {
+    const findOptions = new Search({
+      filter,
+      category,
+      sex,
+      date,
+      page,
+      limit,
+      sort,
+    });
 
-    if (date) {
-      const currentDate = new Date();
-      const dateFields = date.split(",");
+    const notices = await Notice.find(findOptions.getNoticesSearchOptions())
+      .skip(findOptions.getPage())
+      .limit(findOptions.getLimit())
+      .sort(findOptions.getSort("date"));
 
-      if (dateFields.length > 1) {
-        const dates = dateFields.map((item) => {
-          if (item < 1) return currentDate.setMonth(currentDate.getMonth() - 3);
-          return currentDate.setFullYear(currentDate.getFullYear() - item);
-        });
-        const from = Math.max(...dates);
-        findOptions.date = { $lte: from };
-      } else {
-        if (date < 1 && date > 0) {
-          const from = currentDate.setMonth(currentDate.getMonth() - 3);
-          const to = currentDate.setFullYear(currentDate.getFullYear() - 1);
-          findOptions.date = { $lte: from, $gte: to };
-        } else {
-          const from = currentDate.setFullYear(
-            currentDate.getFullYear() - date
-          );
-          findOptions.date = { $lte: from };
-        }
-      }
-    }
-
-    if (category) {
-      findOptions.category = category;
-    }
-
-    if (sex) {
-      findOptions.sex = sex.split(",");
-    }
-
-    const notices = await Notice.find(findOptions)
-      .skip(perPage)
-      .limit(limit)
-      .sort({
-        date: sort,
-      });
-
-    const total = await Notice.count(findOptions);
+    const total = await Notice.count(findOptions.getNoticesSearchOptions());
 
     return { notices, total };
   }
